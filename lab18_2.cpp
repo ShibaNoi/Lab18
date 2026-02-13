c7a982c3fa0cbb3ce73e43d468560e69f6d32ca1
ShibaNoi <evergreen19832@gmail.com>
2026-02-13 23:18:59 +0700

    lab18_2.cpp completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -2,11 +2,37 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
+double overlap(const Rect *A, const Rect *B){
+    double ALeft  = A->x;
+    double ARight = A->x + A->w;
+    double BLeft  = B->x;
+    double BRight = B->x + B->w;
+    
+    double ATop    = A->y;
+    double ABottom = A->y - A->h;
+    double BTop    = B->y;
+    double BBottom = B->y - B->h;
 
+    double left, right, bottom, top;
 
+    if (ALeft > BLeft) left = ALeft;
+    else left = BLeft;
 
+    if (ARight < BRight) right = ARight;
+    else right = BRight;
+
+    if (ABottom > BBottom) bottom = ABottom;
+    else bottom = BBottom;
+
+    if (ATop < BTop) top = ATop;
+    else top = BTop;
+
+    double overlapW = right - left;
+    double overlapH = top - bottom;
+    
+    if (overlapW <= 0 || overlapH <= 0) return 0;
+    return overlapW * overlapH;
 }
